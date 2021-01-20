4fe49528f91ad8da0c2c7b1e520d8541dc9c438e
csetowhid <towhidhasang1@gmail.com>
2021-01-20 12:08:52 +0600

    Change Password

--- top.php
+++ top.php
@@ -126,10 +126,37 @@ $meta_title='Contact Us';
                                     </div>
                 <div class="header__account">
                     <?php if(isset($_SESSION['USER_LOGIN'])) {
-echo '<a href="logout.php">Logout</a> <a href="my_order.php">My Order</a>';
-                                        }else{
-                echo '<a href="login.php">Login/Register</a>';
-                                        }
+                        ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="profile.php">Profile</a>
+          <a class="dropdown-item" href="my_order.php">Order</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="logout.php">Logout</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<?php
+}else{
+    echo '<a href="login.php" class="mr15">Login/Register</a>';
+}
+
+// echo '<a href="logout.php">Logout</a> <a href="my_order.php">My Order</a>';
+//                                         }else{
+//                 echo '<a href="login.php">Login/Register</a>';
+//                                         }
                                          ?>
                                         
                                     </div>
